391c87435fad07654706820e41a237a567c1fc1c
Konstantin Belousov <kib@freebsd.org>
2017-10-04 11:56:35 +0300

    WIP server part.

--- timestamp.cc
+++ timestamp.cc
@@ -1,4 +1,5 @@
 #include <sys/types.h>
+#include <sys/param.h>
 #include <sys/socket.h>
 #include <string.h>
 #include <netdb.h>
@@ -29,6 +30,19 @@ struct timer_descr {
 	{ "realtime_micro", 	T_REALTIME_MICRO },
 	{ "realtime",		T_REALTIME },
 	{ "monotonic",		T_MONOTONIC },
+};
+
+union ts {
+	struct timespec ts;
+	struct timeval tv;
+};
+
+struct packet {
+	int timer;
+	union ts clnt_snd;
+	union ts srv_rcv;
+	union ts srv_snd;
+	union ts clnt_rcv;
 };
 
 static bool
@@ -73,11 +87,11 @@ timestamp_sockopt(int s, enum timer t)
 }
 
 static bool
-do_server(const struct addrinfo *ai, enum timer timer)
+setup_server(const struct addrinfo *ai, enum timer timer, int& s)
 {
 	int error;
 
-	int s = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
+	s = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
 	if (s == -1) {
 		error = errno;
 		std::cerr << "socket: " << strerror(error) << std::endl;
@@ -96,11 +110,11 @@ do_server(const struct addrinfo *ai, enum timer timer)
 }
 
 static bool
-do_client(const struct addrinfo *ai, enum timer timer)
+setup_client(const struct addrinfo *ai, enum timer timer, int& s)
 {
 	int error;
 
-	int s = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
+	s = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
 	if (s == -1) {
 		error = errno;
 		std::cerr << "socket: " << strerror(error) << std::endl;
@@ -119,6 +133,37 @@ do_client(const struct addrinfo *ai, enum timer timer)
 }
 
 static void
+server_loop(int s)
+{
+	int error;
+
+	for (;;) {
+		struct msghdr m{};
+		struct iovec v[1];
+		struct packet p;
+		char control_buf[1024];
+
+		v[0].iov_base = &p;
+		v[0].iov_len = sizeof(p);
+		m.msg_iov = v;
+		m.msg_iovlen = nitems(v);
+		m.msg_control = control_buf;
+		m.msg_controllen = sizeof(control_buf);
+		error = recvmsg(s, &m, 0);
+		if (error == -1) {
+			error = errno;
+			std::cerr << "recvmsg: " << strerror(error) << std::endl;
+			continue;
+		}
+	}
+}
+
+static void
+client_loop(int s)
+{
+}
+
+static void
 usage()
 {
 	std::cerr << "Usage: timestamp -c|-s [-h address] [-p port] [-t timer]\n";
@@ -130,7 +175,7 @@ main(int argc, char *argv[])
 {
 	const char *hostname = NULL, *servname = NULL;
 	struct addrinfo *ai = NULL;
-	int c, error;
+	int c, error, s;
 	enum mode mode = M_UNKNOWN;
 	enum timer timer = T_UNKNOWN;
 
@@ -191,7 +236,7 @@ main(int argc, char *argv[])
 	case M_SERVER:
 		for (const struct addrinfo *cai = ai; cai != NULL;
 		    cai = cai->ai_next) {
-			if (do_server(cai, timer)) {
+			if (setup_server(cai, timer, s)) {
 				res = 0;
 				break;
 			}
@@ -200,7 +245,7 @@ main(int argc, char *argv[])
 	case M_CLIENT:
 		for (const struct addrinfo *cai = ai; cai != NULL;
 		    cai = cai->ai_next) {
-			if (do_client(cai, timer)) {
+			if (setup_client(cai, timer, s)) {
 				res = 0;
 				break;
 			}
@@ -209,5 +254,18 @@ main(int argc, char *argv[])
 	default:
 		break;
 	}
-	return (res);
+	if (res != 0)
+		return (res);
+
+	switch (mode) {
+	case M_SERVER:
+		server_loop(s);
+		break;
+	case M_CLIENT:
+		client_loop(s);
+		break;
+	default:
+		break;
+	}
+	return (0);
 }
